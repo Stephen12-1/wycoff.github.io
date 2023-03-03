e0e3f3ff2ed6d33c5e1deba42992353742de6b85
eric <sensityun1999@gmail.com>
2023-03-03 18:48:49 +0000

    peace

--- assets/js/main.js
+++ assets/js/main.js
@@ -191,6 +191,38 @@
     }
   });
 
+   /**
+   * Hero type effect
+   */
+   const typed = select('.typed')
+   if (typed) {
+     let typed_strings = typed.getAttribute('data-typed-items')
+     typed_strings = typed_strings.split(',')
+     new Typed('.typed', {
+       strings: typed_strings,
+       loop: true,
+       typeSpeed: 100,
+       backSpeed: 50,
+       backDelay: 2000
+     });
+   }
+
+   const carousel = document.querySelector('.carousel');
+const carouselItems = document.querySelector('.carousel-items');
+const carouselItemWidth = carouselItems.querySelector('.carousel-item').offsetWidth;
+let currentIndex = 0;
+
+function goToIndex(index) {
+  currentIndex = index;
+  carouselItems.style.transform = `translateX(${-carouselItemWidth * currentIndex}px)`;
+}
+
+setInterval(() => {
+  const nextIndex = (currentIndex + 1) % carouselItems.children.length;
+  goToIndex(nextIndex);
+}, 3000);
+
+
   /**
    * Skills animation
    */
